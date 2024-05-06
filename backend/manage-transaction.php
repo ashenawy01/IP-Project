6ff075998747d8e8e0bc961f71d5acc75c1fba2e
ALMLYGY <karim219226@bue.edu.eg>
2024-05-06 23:19:18 +0300

    k

---
+++ backend/manage-transaction.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'db_connection.php';
+
+class transaction {
+    $sql = "SELECT * FROM users WHERE type = 'admin'";
+    $result = $conn->query($sql); 
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo ' <tr>
+            <td>'.$row["SSN"].'</td>
+            <td>'.$row["name"].'</td>
+            <td>'.$row["email"].'</td>
+            <td>'.$row["username"].'</td>
+            <td>'.$row["dateOfBirth"].'</td>
+          </tr>';
+        }
+    }
+}
+
+
+?>
